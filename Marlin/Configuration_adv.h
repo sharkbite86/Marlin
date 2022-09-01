cd67b87f22a5f366e0ca1224730dcb73bb5fbf75
InsanityAutomation <d.menzel@insanityautomation.com>
2022-09-01 14:35:45 -0400

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -839,7 +839,9 @@
 #ifdef Z2_DRIVER_TYPE
   //#define INVERT_Z2_VS_Z_DIR        // Z2 direction signal is the opposite of Z
 
-  //#define Z_MULTI_ENDSTOPS          // Other Z axes have their own endstops
+  #if DISABLED(BLTOUCH)
+    #define Z_MULTI_ENDSTOPS          // Other Z axes have their own endstops
+  #endif
   #if ENABLED(Z_MULTI_ENDSTOPS)
     #define Z2_USE_ENDSTOP   _XMAX_   // Z2 endstop board plug. Don't forget to enable USE_*_PLUG.
     #define Z2_ENDSTOP_ADJUSTMENT 0   // Z2 offset relative to Y endstop
