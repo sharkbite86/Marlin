56ee00f3540e1be827d114bc79d5c7e8e4a96c2b
InsanityAutomation <d.menzel@insanityautomation.com>
2023-02-24 15:34:06 -0500

    Minor tweak, fix fan speed input

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -320,7 +320,7 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD  20               // Seconds
+  #define WATCH_TEMP_PERIOD  30               // Seconds
   #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
 #endif
 
@@ -328,8 +328,13 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD        20 // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     4 // Degrees Celsius
+  #if ENABLED(MachineTLD6)
+    #define THERMAL_PROTECTION_BED_PERIOD        60 // Seconds
+    #define THERMAL_PROTECTION_BED_HYSTERESIS     2 // Degrees Celsius
+  #else
+    #define THERMAL_PROTECTION_BED_PERIOD        40 // Seconds
+    #define THERMAL_PROTECTION_BED_HYSTERESIS     4 // Degrees Celsius
+  #endif
 
   /**
    * As described above, except for the bed (M140/M190/M303).
