856dcc7fe85a5978695c0341b2cb9f57c252e5d7
InsanityAutomation <d.menzel@insanityautomation.com>
2024-01-27 11:48:29 -0500

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "DW7.4.8"
+#define SHORT_BUILD_VERSION "DW7.4.9"
 
 /**
  * Verbose version identifier which should contain a reference to the location
